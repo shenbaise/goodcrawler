53d6f5185fd6b359c8cbcc53f014a13a3b42720e
shenbaise <shenbaise1001@126.com>
2013-07-29 23:27:57 +0800

    添加web功能
    Signed-off-by: shenbaise <shenbaise1001@126.com>

---
+++ src/main/java/org/sbs/web/ContextListener.java
@@ -0,0 +1,39 @@
+package org.sbs.web;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class ContextListener
+ *
+ */
+@WebListener
+public class ContextListener extends ServletContextEvent implements ServletContextListener {
+
+	private static final long serialVersionUID = -2217410422446016104L;
+
+	/**
+     * @see ServletContextEvent#ServletContextEvent(ServletContext)
+     */
+    public ContextListener(ServletContext source) {
+        super(source);
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce) {
+        // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce) {
+        // TODO Auto-generated method stub
+    }
+	
+}
